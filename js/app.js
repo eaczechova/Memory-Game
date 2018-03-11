dc69eed23ef5ee51b384eefc34046e3fe63cf1ce
Ewa Czech <eaprojekty@gmail.com>
2018-03-11 13:56:44 +0100

    add function that flips and match cards, with comments

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ function shuffle(array) {
 
   return array;
 }
+
 shuffle(cardList);
 
 /*
@@ -59,4 +60,73 @@ shuffle(cardList);
    ulElement.appendChild(fragment);
  }
 
- gameBuilder()
+ gameBuilder();
+
+ const deck = document.querySelector('.deck');
+ let pointCounter = 0;
+ let moveCounter = 0;
+
+ let classArray = [];
+ let arrayOfUnmached = [];
+
+ const starsList = document.querySelector('.stars');
+ const stars = document.getElementsByClassName('fa-star');
+
+ const modalBox = document.querySelector('.modal');
+ const newGameButton = document.querySelector('.new-game');
+
+ let arrayOfOpenCards = [];
+
+
+ /* DISPLAY CARD AND MATCH FUNCTION
+
+ * the function operatetes on elements that DO NOT have class .match
+ * if an element cointains class .open, thn is placed in the array of open elements
+ * there is an array, classArray, that holds target element's child class name
+ * as each <i> element has class of .fa (font-awesome class name), the function slice first 3 characters
+ * if classes of two open cards match, point counter is increased by one, elements get class .match added
+ * if classes of two open cards do not match, they are flipped back and classes .open .show are removed
+ */
+
+ function displayCardSymbolAndMatch(e) {
+   if (e.target.classList.contains('match')) {
+     e.preventDefault();
+   } else {
+       if (e.target.tagName === 'LI') {
+          moveCounter++;
+          e.target.style.transform = 'rotateY(180deg)';
+          e.target.backfaceVisibility = 'hidden';
+          e.target.classList.add('open', 'show');
+       }
+   }
+
+   if (e.target.classList.contains('open')) {
+     arrayOfOpenCards.push(e.target);
+     classArray.push(e.target.lastChild.className.slice(3));
+   }
+
+   if (classArray.length === 2 && classArray[0] === classArray[1]) {
+     pointCounter++;
+     let list = document.getElementsByClassName(classArray[0]);
+     setTimeout(function() {
+       for(let i = 0; i < list.length; i++) {
+           list[i].parentNode.classList = 'card match';
+           list[i].parentNode.style.animation = 'match 1s';
+         }
+       }, 1000);
+       classArray = [];
+       arrayOfOpenCards = [];
+     } else if (classArray.length === 2 && classArray[0] !== classArray[1]) {
+         for (let i = 0; i < arrayOfOpenCards.length; i++) {
+           if (arrayOfOpenCards[i].classList.contains('open')) {
+             setTimeout(function() {
+               arrayOfOpenCards[i].style.transform = 'rotateY(360deg)';
+               arrayOfOpenCards[i].backfaceVisibility = 'hidden';
+               arrayOfOpenCards[i].classList.remove('open', 'show');
+             }, 1500);}
+         classArray = [];
+         }
+       }
+ }
+
+ deck.addEventListener("click", displayCardSymbolAndMatch);
